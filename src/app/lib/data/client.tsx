511505894b1f98d63e35a401b1b93496180db1e2
Kendall Castilla <kjcastilla+dev@gmail.com>
2024-02-29 17:44:40 -0500

    Implement Supabase integration/querying

--- src/app/lib/data/client.tsx
+++ src/app/lib/data/client.tsx
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from "./definitions";
 
+require('dotenv');
 
-export const supabaseClient = createClient<Database>(
+export const supabase = createClient<Database>(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!
 )
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,24 +2,29 @@ import { fetchPostData } from "@/app/lib/data/actions";
 import { entries } from "@/app/lib/data/placeholder-data";
 import parse from 'html-react-parser';
 import './styles.css'
+import { supabase } from "@/app/lib/data/client";
+import { notFound } from "next/navigation";
+
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
-    // const {
-    //     title, 
-    //     writeDate, 
-    //     publishDate, 
-    //     interaction, 
-    //     content
-    // } = await fetchPostData(id);
+    const { data } = await supabase
+        .from('Posts')
+        .select()
+        .match({ id })
+        .single()
 
-    const {
-        title,
-        writeDate,
-        publishDate,
-        interaction,
-        content,
-    } = entries[0];
+    if (!data) {
+        notFound();
+    }
+
+    // const {
+    //     title,
+    //     writeDate,
+    //     publishDate,
+    //     interaction,
+    //     content,
+    // } = entries[0];
 
     return (
         <div id="postFull" className="flex flex-col items-center w-full h-full overflow-y-auto ">
@@ -29,7 +34,7 @@ export default async function Page({ params }: { params: { id: string } }) {
                 <div id="postTitleHeaderDiv" className="w-full
                         md:flex md:flex-col-reverse md:w-[70%] md:h-full">
                     <h1 id="postTitle" className="text-5xl w-full h-full md:h-fit">
-                        {title}
+                        {data.title}
                     </h1>
                 </div>
                 <div id="postHeaderDivider" className="w-full h-[3em] md:w-0 md:h-0"></div>
@@ -37,15 +42,15 @@ export default async function Page({ params }: { params: { id: string } }) {
                         md:flex md:flex-col-reverse md:w-[30%] md:h-full">
                     <h2 id="postSubtitle" className="text-xl w-full h-fit
                                 md:text-end">
-                        Published: {publishDate}<br />Written: {writeDate}
+                        Published: {data.publish_date}<br />Written: {data.write_date}
                     </h2>
                 </div>
             </div>
             <div id="postContentDiv" className="mb-1 w-[90%] h-0 text-md md:text-lg md:w-[80%] md:mt-[5%]">
                 <p id="content" className="w-full h-auto">
-                    {parse(content)}
+                    {parse(data.content!)}
                 </p>
-            <div id="bottomOfPage" className="w-[90%] md:h-5">&nbsp;</div>
+                <div id="bottomOfPage" className="w-[90%] md:h-5">&nbsp;</div>
             </div>
         </div>
     )
