7d9ff2cb9172c904af5718b1767ad5f8480b1385
Kendall Castilla <kjcastilla+dev@gmail.com>
2024-06-03 14:12:59 -0400

    Quote page now displays latest quote or lyric added to table

--- src/app/lib/quote/functions.tsx
+++ src/app/lib/quote/functions.tsx
@@ -7,7 +7,7 @@ export default async function getQuote() {
    const { data, error } = await supabase
       .from('quotes')
       .select('author, quote, work')
-      .order('created_at', { ascending: true })
+      .order('created_at', { ascending: false })
       .limit(1);
 
    return { author: data![0]?.author, quote: data![0]?.quote, work: parse(data![0]?.work!) };
